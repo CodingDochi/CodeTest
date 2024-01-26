2ab60d595e2f76a577673f0adc9d19852e7aff4d
Dev-Hammy <cheers7129@gmail.com>
2024-01-26 22:46:45 +0900

    execution time

---
+++ baekjoon/src/main/java/algorithmLesson/executionTime/P_24263/Main.java
@@ -0,0 +1,28 @@
+package algorithmLesson.executionTime.P_24263;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    /*
+
+    아래 알고리즘에서 반복문은 입력의 크기 n 만큼 수행된다. 반복문이 중첩되지 않으므로 차수는 1이다.
+
+    MenOfPassion(A[], n) {
+    sum <- 0;
+    for i <- 1 to n
+        sum <- sum + A[i]; # 코드1
+    return sum;
+    }
+     */
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n);
+        System.out.println(1);
+
+    }
+}
