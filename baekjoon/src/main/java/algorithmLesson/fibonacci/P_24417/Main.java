7ab92f49dd8f3aa28df18d772beb3725197f8ac3
Dev-Hammy <cheers7129@gmail.com>
2024-02-06 13:12:46 +0900

    tail call optimization, memoization, modular etc

---
+++ baekjoon/src/main/java/algorithmLesson/fibonacci/P_24417/Main.java
@@ -0,0 +1,31 @@
+package algorithmLesson.fibonacci.P_24417;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    private int fib_dp(int n) {
+        int minus1 = 1;
+        int num = 1;
+
+        for (int i = 3; i <= n; i++) {
+            int temp = num;
+            num = (minus1 + num) % 1000000007;
+            minus1 = temp;
+        }
+
+        return num;
+    }
+
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int intN = Integer.parseInt(br.readLine());
+
+        System.out.println(fib_dp(intN) + " " + (intN - 2));
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
