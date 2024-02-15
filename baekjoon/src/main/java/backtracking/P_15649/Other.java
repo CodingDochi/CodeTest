846593c34f7ff3451538c2154bc6869df0412669
CodingDochi <cheers7129@gmail.com>
2024-02-15 21:10:22 +0900

    backtrack asc

--- baekjoon/src/main/java/backtracking/P_15649/Other.java
+++ baekjoon/src/main/java/backtracking/P_15649/Other.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+// alphanumeric order
+
 public class Other {
 
     int N;
@@ -37,8 +40,6 @@ public class Other {
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        // 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
-        // alphanumeric order
 
         rest = new int[M];
         visited = new boolean[N];
